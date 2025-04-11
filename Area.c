a10fc1241d72ac614eca1153836d942490948d70
yashDumasia <dumasiayash1897@gmail.com>
2025-04-11 12:56:18 +0530

    Add Area File

---
+++ Area.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// area of square
+
+int main(){
+
+    int side;
+    
+    printf("Enter Side : ");
+    
+    scanf("%d", &side);
+
+    printf("Area of square is = %d \n", side*side);
+    
+    return 0;
+}
