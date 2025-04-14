1eca72fda7e71a178b620e4828d508b3306df3ea
yashDumasia <dumasiayash1897@gmail.com>
2025-04-14 11:52:54 +0530

    Add marks file

---
+++ marks.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    int marks;
+
+    printf("enter your marks : ");
+
+    scanf("%d",&marks);
+
+    if (marks > 33)
+    {
+    printf ("pass \n");
+    }
+
+    else
+    {
+    printf ("fail \n");
+    }
+    
+    return 0;
+
+}
