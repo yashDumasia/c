c547033721bdf555f1d0b6e875b845e1e30bee42
yashDumasia <dumasiayash1897@gmail.com>
2025-04-15 17:02:34 +0530

    add nestedif file

--- Q.5.c
+++ Q.5.c
@@ -1,15 +1,13 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main() {
+  // if even output is = 1 or if odd output is = 0
+  int x;
 
-    // if even output is = 1 or if odd output is = 0 
-    int x;
+  printf("enter any number : ");
 
-    printf("enter any number : ");
+  scanf("%d", &x);
 
-    scanf("%d",&x);
+  printf("%d \n", x % 2 == 0);
 
-    printf("%d \n", x % 2 == 0);
-
-    return 0;
-     
+  return 0;
 }
