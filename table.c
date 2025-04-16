a22969c980335b83caecb281a87c536651e2d2d6
yashDumasia <dumasiayash1897@gmail.com>
2025-04-16 20:54:03 +0530

    add table file

---
+++ table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  int n;
+  printf("enter value of n : ");
+  scanf("%d", &n);
+
+  printf("table of %d is given bellow. \n", n);
+
+  //   for (int i = n; i <= 10 * n; i = i + n) {
+  //     printf("%d \n", i);
+  //   }
+
+  for (int i = 1; i <= 10; i++) {
+    printf("%d\n", i * n);
+  }
+  return 0;
+}
