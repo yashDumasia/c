e27f7bb9f632130047a59a19c9a5af7051be2f91
yashDumasia <dumasiayash1897@gmail.com>
2025-04-17 13:35:04 +0530

    some changes in forLoop for testing break

--- forLoop.c
+++ forLoop.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main() {
   for (int i = 0; i <= 100; i = i + 1) {
+    if (i == 51) {
+      break;
+    }
     printf("%d \n", i);
   }
+
+  printf("end\n");
 
   return 0;
 }
