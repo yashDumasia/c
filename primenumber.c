451590272dd45baab95b8bfe614741b7f3d0fd6a
yashDumasia <dumasiayash1897@gmail.com>
2025-04-18 21:03:43 +0530

    add primenumber file

---
+++ primenumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  int n;
+  printf("Enter any number : %d", n);
+  scanf("%d", &n);
+
+  int count = 0;
+  for (int i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      count = count + 1;
+    }
+  }
+
+  if (count == 2) {
+    printf("It is a prime number.\n");
+  } else {
+    printf("It is not a prime number.\n");
+  }
+
+  return 0;
+}
