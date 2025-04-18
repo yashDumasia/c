4c5f6595aa2b11a35044451b8a39607341fa52bd
yashDumasia <dumasiayash1897@gmail.com>
2025-04-18 12:13:51 +0530

    add Q.21 file

---
+++ Q.21.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+int main() {
+  int n;
+  int fact = 1;
+  printf("enter n : ");
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    fact = fact * i;
+  }
+  printf("%d\n", fact);
+
+  return 0;
+}
