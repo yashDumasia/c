798ee6c7d2d8c974791c549fa2b3c8b440c967b8
yashDumasia <dumasiayash1897@gmail.com>
2025-04-25 17:05:19 +0530

    add fibonacci file for find n'th term in fibonacci serise.

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int fib(int n);
+int main() {
+  int n;
+  printf("enter n : ");
+  scanf("%d", &n);
+
+  printf("%dth term of fibonacci serise is : %d\n", n, fib(n - 1));
+
+  return 0;
+}
+int fib(int n) {
+  if (n == 0) {
+    return 0;
+  }
+
+  if (n == 1) {
+    return 1;
+  }
+  int fibNm1 = fib(n - 1);
+  int fibNm2 = fib(n - 2);
+  int fibN = fibNm1 + fibNm2;
+
+  return fibN;
+}
