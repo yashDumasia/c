abf87ead92b357b34c8014683587bfa500105f10
yashDumasia <dumasiayash1897@gmail.com>
2025-05-12 12:26:36 +0530

    Add fgets and puts in Q.48 file

--- Q.48.c
+++ Q.48.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main() {
-  char firstName[50];
-  scanf("%s", firstName);
-  printf("Your Name is %s\n", firstName);
+  char fullName[100];
+  //   scanf("%s", firstName);
+  //   printf("Your Name is %s\n", firstName);
+
+  fgets(fullName, 100, stdin);
+  puts(fullName);
+  return 0;
 }
