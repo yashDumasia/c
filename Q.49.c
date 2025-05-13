327e3284f4fcd87cec3dee80a259c8e46c127127
yashDumasia <dumasiayash1897@gmail.com>
2025-05-13 12:05:53 +0530

    some change in Q.49 file : add strlen

--- Q.49.c
+++ Q.49.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 int printcount(char arr[]);
 int main() {
   char name[100];
   fgets(name, 100, stdin);
+  int lenth = strlen(name) - 1;
 
   printf("lenth is : %d\n", printcount(name));
-
+  printf("lenth is : %d\n", lenth);
   return 0;
 }
 int printcount(char arr[]) {
