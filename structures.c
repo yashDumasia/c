201f4955a9db49462837a65019542cf9ba84357f
yashDumasia <dumasiayash1897@gmail.com>
2025-05-15 15:44:18 +0530

    some changes : add pointer in structures file

--- structures.c
+++ structures.c
@@ -8,12 +8,8 @@ struct student {
 };
 
 int main() {
-  struct student arr[100];
-  arr[0].roll = 3;
-  arr[0].cgpa = 9.5;
-  strcpy(arr[0].name, "YASH");
-  printf("student name = %s\n", arr[0].name);
-  printf("student roll no. = %d\n", arr[0].roll);
-  printf("student cgpa = %f\n", arr[0].cgpa);
+  struct student y = {3, "YASH", 9.5};
+  struct student *ptr = &y;
+  printf("%d\n", (*ptr).roll);
   return 0;
 }
