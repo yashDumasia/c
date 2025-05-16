ee5bd332386b6c123e69997d93d10a5fd3dc0666
yashDumasia <dumasiayash1897@gmail.com>
2025-05-16 13:30:05 +0530

    some changes : add function in structures file

--- structures.c
+++ structures.c
@@ -7,9 +7,16 @@ struct student {
   float cgpa;
 };
 
+void printInfo(struct student y);
+
 int main() {
-  struct student y = {3, "YASH", 9.5};
-  struct student *ptr = &y;
-  printf("%d\n", (*ptr).roll);
+  struct student y = {3, "YASH DUMASIA", 9.7};
+  printInfo(y);
+
   return 0;
+}
+void printInfo(struct student y) {
+  printf("NAME IS : %s\n", y.name);
+  printf("ROLL NO. IS : %d\n", y.roll);
+  printf("CGPA IS : %f\n", y.cgpa);
 }
