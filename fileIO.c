2c43302116c1aa77697752463c06dabcd9657b76
yashDumasia <dumasiayash1897@gmail.com>
2025-05-17 15:00:42 +0530

    some changes in fileIO file : write in test.txt file

--- fileIO.c
+++ fileIO.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main() {
   FILE *fptr;
-  fptr = fopen("test.txt", "r");
-  char ch[100];
-  fscanf(fptr, "%s", &ch);
-  printf("%s\n", ch);
+  fptr = fopen("test.txt", "w");
+  fprintf(fptr, "%s", "DUMASIA");
 
   fclose(fptr);
   return 0;
