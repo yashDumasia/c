2f6e730a669db95c1445615d02001ab671c97477
yashDumasia <dumasiayash1897@gmail.com>
2025-05-19 11:34:59 +0530

    some changes in fileIO file : add fgetc

--- fileIO.c
+++ fileIO.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 int main() {
   FILE *fptr;
-  fptr = fopen("test.txt", "a");
-  fprintf(fptr, "%s", "DUMASIA");
+  fptr = fopen("test.txt", "r");
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c", fgetc(fptr));
+  printf("%c\n", fgetc(fptr));
 
   fclose(fptr);
   return 0;
