1fdb36ff0977246544ed62c6ca81aa4378335d77
yashDumasia <dumasiayash1897@gmail.com>
2025-05-19 11:38:01 +0530

    some changes in fileIO file : add fputc

--- fileIO.c
+++ fileIO.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 int main() {
   FILE *fptr;
-  fptr = fopen("test.txt", "r");
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c", fgetc(fptr));
-  printf("%c\n", fgetc(fptr));
+  fptr = fopen("test.txt", "w");
+  fputc('D', fptr);
+  fputc('U', fptr);
+  fputc('M', fptr);
+  fputc('A', fptr);
+  fputc('S', fptr);
+  fputc('I', fptr);
+  fputc('A', fptr);
 
   fclose(fptr);
   return 0;
